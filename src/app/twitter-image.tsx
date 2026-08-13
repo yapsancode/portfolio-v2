8be5387f1d789919c9501b1eec7569c2a92b2b7d
Isyraf <isyrafsan97@gmail.com>
2026-08-14 02:39:55 +0800

    delete. we redo everything

--- src/app/twitter-image.tsx
+++
@@ -1,3 +0,0 @@
-// X/Twitter reuses the exact same card as Open Graph. Re-export so there's one
-// source of truth for the share image.
-export { default, alt, size, contentType } from "./opengraph-image";
